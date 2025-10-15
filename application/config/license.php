6c863f795cbc6c17574be1a121bfd015eab6a624
Umidjon1990 <umidjonabdurayimov04@gmail.com>
2025-10-15 08:01:31 -0700

    Initial Smart School upload

---
+++ application/config/license.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+$config['envato_market_purchase_code'] = '';
+$config['envato_market_username'] = '';
+$config['SSLK'] = 'USGQ77-8ZL5B9-F94G8G-di8xQ0Zrd0lPNGUzcUNPVVBBZDdFSWU1eDJUSkEza3RuemhXdExaeGpSTT0=';
+$config['app_ver'] = 0;
+$config['addon_prod'] = array('BNYPU-RA75K-N6NCG-LPLWA');
+$config['addon_ver'] = array('Q1AwWHhnWUhKT2h4cGl2WEptYXJYbTRiSlZlN1pob0lMUVNaNmo1UFFXRGM0Q0F4SW1zV0RHdmN6alpzSlE3Tzo6iAfshkLIWO3RfFMm+697AQ==');
